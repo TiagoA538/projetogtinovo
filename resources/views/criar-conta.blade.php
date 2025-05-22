c5576e4b2d61072718e51520a4a8a375453928fc
TiagoA538 <tiagomucukas@gmail.com>
2025-05-22 10:53:40 -0300

    nao vou copiar do colega

---
+++ resources/views/criar-conta.blade.php
@@ -0,0 +1,30 @@
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+<x-topo></x-topo>
+<section class="bg-white dark:bg-gray-900">
+  <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Crie sua conta</h2>
+      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          Crie sua conta no melhor sistema já desenvolvido por alunos do curso de GTI. Acesse o conteúdo e faça parte dessa história
+      </p>
+
+      <form action="{{ route('salva-conta') }}" method="POST" class="space-y-8">
+        @csrf
+        <div>
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Seu email</label>
+            <input name="email" type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5" placeholder="name@exemplo.com" required>
+        </div>
+        <div>
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Seu Nome</label>
+            <input name="name" type="text" id="name" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300" placeholder="Seu nome completo" required>
+        </div>
+        <div>
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Senha</label>
+            <input name="password" type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full p-2.5" required>
+        </div>
+
+        <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800">Criar Conta</button>
+
+        <p class="text-center text-sm text-gray-500">Já tem uma conta? <a href="{{ route('login') }}" class="text-blue-600 hover:underline">Entrar</a></p>
+      </form>
+  </div>
+</section>
---
+++ routes/web.php
@@ -0,0 +1,83 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+Route::get('/', function () {
+    return view('inicio');
+})->name('inicio');
+
+// Tela de cadastro
+Route::get('/criar-conta', function () {
+    return view('criar-conta');
+});
+
+// Salva usuário
+Route::post('/salva-conta', function (Request $request) {
+    $user = new User();
+    $user->name = $request->name;
+    $user->email = $request->email;
+    $user->password = Hash::make($request->password); // senha criptografada
+    $user->save();
+
+    return redirect(route('login')); // redireciona ao login
+})->name('salva-conta');
+
+// Tela de login
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+// Validação de login
+Route::post('/logar', function (Request $request) {
+    $credentials = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
+
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
+        return redirect()->intended('dashboard');
+    }
+
+    return back()->withErrors([
+        'email' => 'O email e senha digitados não são válidos',
+    ])->onlyInput('email');
+})->name('logar');
+
+// Dashboard (pós login)
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard')->middleware('auth');
+
+// Rotas de teste
+Route::get('/teste', function () {
+    return 'O código foi testado';
+});
+
+Route::get('/usuario/{nome}', function ($nome) {
+    return 'o usuário atual é:'.$nome;
+});
+
+Route::get('/soma/{num1}/{num2}', function ($num1, $num2) {
+    return 'a soma é: '.$num1 + $num2;
+});
+
+Route::get('/divisão/{num1}/{num2}', function ($num1, $num2) {
+    return 'a divisão é: '.$num1 / $num2;
+});
+
+Route::get('/subtração/{num1}/{num2}', function ($num1, $num2) {
+    return 'a subtração é: '.$num1 - $num2;
+});
+
+Route::get('/multiplicação/{num1}/{num2}', function ($num1, $num2) {
+    return 'a multiplicação é: '.$num1 * $num2;
+});
+
+Route::get('/cadastra-equipe', function () {
+    return view('cadastra-equipe');
+})->name('cadastra-equipe');
